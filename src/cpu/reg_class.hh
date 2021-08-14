b56d6d410080ac52732115c16718f0b9e7ef7a14
Gabe Black <gabe.black@gmail.com>
2021-08-14 02:50:25 -0700

    cpu,arch-arm: Use a sentry class valid for invalid RegIds.
    The default constructor for RegId would initialize it with the
    IntRegClass and register index 0. This is arbitrary and
    indistinguishable from a real ID to the first integer register.
    
    Instead, add a new class type constant InvalidRegClass, and use that to
    initialize an otherwise uninitialized RegId.
    
    Also, fill out some enums that needed to handle that value to silence
    compiler warnings.
    
    Change-Id: I3b58559f41adc1da5f661121225dbd389230e3af

--- src/cpu/reg_class.hh
+++ src/cpu/reg_class.hh
@@ -42,6 +42,7 @@
 #define __CPU__REG_CLASS_HH__
 
 #include <cstddef>
+#include <limits>
 #include <string>
 
 #include "base/cprintf.hh"
@@ -63,7 +64,8 @@ enum RegClassType
     VecElemClass,
     VecPredRegClass,
     CCRegClass,         ///< Condition-code register
-    MiscRegClass        ///< Control (misc) register
+    MiscRegClass,       ///< Control (misc) register
+    InvalidRegClass = -1
 };
 
 class RegId;
@@ -136,7 +138,7 @@ class RegId
     friend struct std::hash<RegId>;
 
   public:
-    RegId() : RegId(IntRegClass, 0) {}
+    RegId() : RegId(InvalidRegClass, 0) {}
 
     explicit RegId(RegClassType reg_class, RegIndex reg_idx)
         : regClass(reg_class), regIdx(reg_idx), numPinnedWrites(0)
@@ -235,7 +237,7 @@ class PhysRegId : private RegId
     bool pinned;
 
   public:
-    explicit PhysRegId() : RegId(IntRegClass, -1), flatIdx(-1),
+    explicit PhysRegId() : RegId(InvalidRegClass, -1), flatIdx(-1),
                            numPinnedWritesToComplete(0)
     {}
 
